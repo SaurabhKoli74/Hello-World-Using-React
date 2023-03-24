cf414ba1530b4cc93d673438086a8da9b93fd5c5
Saurabh <saurabh.koli7420@gmail.com>
2023-03-24 19:17:44 +0530

    Hello World in React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name="Saurabh";
+  const isLoggedIn=true;
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {!isLoggedIn && <h1>Hello World</h1>}
+          {isLoggedIn && "Hello "+name}
         </p>
         <a
           className="App-link"
